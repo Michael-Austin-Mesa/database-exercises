77a62e54456dfa4b31f60e3d60fc06c212c58039
Michael Mesa <michael.austin.mesa@gmail.com>
2022-09-07 16:04:19 -0500

    updated exercises answers during lesson

--- table_exercises.sql
+++ table_exercises.sql
@@ -1,5 +1,12 @@
 USE employees;
 SHOW Tables;
+
+/* 
+What different data types are present on this table? 
+Answer: int, date, varchar(), enum(), date
+*/
+DESCRIBE employees;
+
 /* 
 Which table(s) do you think contain a numeric type column?
 Answer: dept_emp, dept_manager, employees, salaries, titles
@@ -15,4 +22,15 @@ Answer: The departments table has a column for dept_no which is shared in the de
 In the dept_emp and dept_manager tables, there is a column named emp_no which is shared with the employees table. 
 The employees are organized by dept_no, then organized by emp_no. The tables are referenced to one another but not directly linked.
 */
-DESCRIBE dept_manager;
+SHOW CREATE TABLE dept_manager;
+/* CREATE TABLE `dept_manager` (
+  `emp_no` int NOT NULL,
+  `dept_no` char(4) NOT NULL,
+  `from_date` date NOT NULL,
+  `to_date` date NOT NULL,
+  PRIMARY KEY (`emp_no`,`dept_no`),
+  KEY `dept_no` (`dept_no`),
+  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+*/
