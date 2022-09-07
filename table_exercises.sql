24285eee683c04462abdeaf7de37faf9ef9803d4
Michael Mesa <michael.austin.mesa@gmail.com>
2022-09-07 15:18:03 -0500

    updated answer to exercise 9

--- table_exercises.sql
+++ table_exercises.sql
@@ -13,6 +13,6 @@ Answer: dept_emp, dept_manager, employees, salaries, titles
 What is the relationship between the employees and the departments tables?
 Answer: The departments table has a column for dept_no which is shared in the dept_emp and dept_mananger tables.
 In the dept_emp and dept_manager tables, there is a column named emp_no which is shared with the employees table. 
-The employees are organized by dept_no, then organized by emp_no.
+The employees are organized by dept_no, then organized by emp_no. The tables are referenced to one another but not directly linked.
 */
 DESCRIBE dept_manager;
